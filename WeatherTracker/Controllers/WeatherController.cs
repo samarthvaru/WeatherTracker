6fe000e4682e01776182d58cb3df3524e0e697b3
Samarth Varu <samarthvaru111@gmail.com>
2024-01-08 18:41:00 -0500

    Add project files.

---
+++ WeatherTracker/Controllers/WeatherController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WeatherTracker.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WeatherTracker.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public WeatherController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string cityName)
+        {
+            var client = _clientFactory.CreateClient();
+            string apiKey = "441d1ef463ebf1398130cc04ca151c87";
+            string apiUrl = $"http://api.weatherstack.com/current?access_key={apiKey}&query={cityName}";
+
+            var response = await client.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                WeatherResponse weather = JsonConvert.DeserializeObject<WeatherResponse>(json);
+
+                // Pass the weather object to the view
+                return View("WeatherResult",weather);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+    }
+}
---
+++ WeatherTracker/Models/WeatherResponse.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherTracker.Models
+{
+    public class WeatherResponse
+    {
+        public Request Request { get; set; }
+        public Location Location { get; set; }
+        public Current Current { get; set; }
+    }
+
+    public class Request
+    {
+        public string Type { get; set; }
+        public string Query { get; set; }
+        // Other properties...
+    }
+
+    public class Location
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        // Other properties...
+    }
+
+    public class Current
+    {
+        [JsonProperty("observation_time")]
+        public string ObservationTime { get; set; }
+        public int Temperature { get; set; }
+
+        [JsonProperty("weather_descriptions")]
+        public List<string> WeatherDescriptions { get; set; }
+        // Other properties...
+    }
+}
